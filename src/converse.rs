132a8d8930fe0edc4f529438c4669b71e529aa7b
inszee <cto@inszee.com>
2023-10-23 21:19:11 +0800

    FIX: 修复send_role_message_streaming 接口判断history是否超过token限制

--- src/converse.rs
+++ src/converse.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use tokio::{fs::File, io::AsyncWriteExt};
+use tiktoken_rs::{get_chat_completion_max_tokens, ChatCompletionRequestMessage};
 
 #[cfg(feature = "functions")]
 use crate::functions::{
@@ -166,6 +167,30 @@ impl Conversation {
         }
     }
 
+    /// calculate current request message ticktoken
+    fn get_retain_completion_max_tokens(&mut self) -> anyhow::Result<usize> {
+        let model = self.client.config.engine.to_string();
+        let messages: Vec<ChatCompletionRequestMessage> = self
+            .history
+            .iter()
+            .map(|item| {
+                let role_name = match item.role {
+                    Role::System => "system".to_string(),
+                    Role::Assistant => "assistant".to_string(),
+                    Role::User => "user".to_string(),
+                    Role::Function => "function".to_string()
+                };
+                ChatCompletionRequestMessage {
+                    content: Some(item.content.clone()),
+                    role: role_name,
+                    name: None,
+                    function_call: None,
+                }
+            })
+            .collect();
+        get_chat_completion_max_tokens(&model, &messages)
+    }
+
     /// Sends a message with specified role to the ChatGPT API and returns the completion response as stream.
     ///
     /// Note, that this method will not automatically save the received message to history, as
@@ -186,6 +211,43 @@ impl Conversation {
             #[cfg(feature = "functions")]
             function_call: None,
         });
+
+        if let Ok(token_count) = self.get_retain_completion_max_tokens() {
+            log::info!("send_message_streaming request tiktoken = {}", token_count);
+            if token_count <= 1024 {
+                // TODO: save user chat history into files.
+                // 给completion max token保留最少1024的长度
+                let mut new_history = vec![];
+                // 保留3轮对话
+                if self.history.len() <= 3 {
+                    log::warn!("send_message_streaming too long for user input...");
+                } else {
+                    // 只保留前3轮对话主题和当前对话
+                    log::warn!("send_message_streaming transcate token for input...");
+                    new_history.push(self.history[0].to_owned());
+                    new_history.push(self.history[1].to_owned());
+                    new_history.push(self.history[2].to_owned());
+                    if new_history.len() == 4 {
+                        new_history.push(self.history[self.history.len() - 1].to_owned());
+                    } else if new_history.len() == 5 {
+                        new_history.push(self.history[self.history.len() - 2].to_owned());
+                        new_history.push(self.history[self.history.len() - 1].to_owned());
+                    }  else if new_history.len() == 6 {
+                        new_history.push(self.history[self.history.len() - 3].to_owned());
+                        new_history.push(self.history[self.history.len() - 2].to_owned());
+                        new_history.push(self.history[self.history.len() - 1].to_owned());
+                    } else {
+                        // 保留多一轮对话(user + assistant)
+                        new_history.push(self.history[self.history.len() - 4].to_owned());
+                        new_history.push(self.history[self.history.len() - 3].to_owned());
+                        new_history.push(self.history[self.history.len() - 2].to_owned());
+                        new_history.push(self.history[self.history.len() - 1].to_owned());
+                    }
+                    self.history.clear();
+                    self.history.extend_from_slice(&new_history);
+                }
+            }
+        }
         let stream = self.client.send_history_streaming(&self.history).await?;
         Ok(stream)
     }
